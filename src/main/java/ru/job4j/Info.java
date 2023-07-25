4995e5833c6f9e621d58c8f2f452fdc0ca57aa30
Fensyk <Dima-fech@yandex.ru>
2023-07-25 15:27:58 +0300

    4.2. Заголовок в файле java [#159036 # [#159036]]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,6 +1,6 @@
 
 public class Info{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("25.07.2023");
     }
 }
--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 
-public class Multiple {
+public class Multiple{
     public static void main(String[] args){
         System.out.println("1*1=1");
         System.out.println("1*2=2");
@@ -11,6 +11,5 @@ public class Multiple {
         System.out.println("1*7=7");
         System.out.println("1*8=8");
         System.out.println("1*9=9");
-        System.out.println("1*10=10");
-    }
+        System.out.println("1*10=10");}
 }
