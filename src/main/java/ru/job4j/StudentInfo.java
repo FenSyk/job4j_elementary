16c76914c91a9697e0e1f6818e65426eb27f7b53
Fensyk <Dima-fech@yandex.ru>
2023-07-25 14:02:08 +0300

    3.3. Фиксация изменений на github [#243564 # [#243564]]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args){
+        System.out.println("Dmitry Fech");
+        System.out.println("14.04.1995");
+}
+}
