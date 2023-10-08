d18fbce34d9f5f35f8720daa8024d186108b93c8
Fensyk <Dima-fech@yandex.ru>
2023-10-08 13:17:26 +0300

    6. Идеальный вес [#41584]

--- src/main/java/ru/job4j/ru/job4j/calculator/calculator/Fit.java
+++ src/main/java/ru/job4j/ru/job4j/calculator/calculator/Fit.java
@@ -7,7 +7,7 @@ public class Fit {
 }
 
     public static double womanWeight(short height) {
-        return height - 110 * 1.15;
+        return height -110 * 1.15;
     }
 
     public static void main(String[] args) {
