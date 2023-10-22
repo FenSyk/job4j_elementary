f23819c725a3da0e9ae57bb86d4792a88129f010
Fensyk <Dima-fech@yandex.ru>
2023-10-22 23:00:10 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+import java.sql.SQLOutput;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java,";
+        System.out.println(idea);
+        String rt = idea + " But i am a newbie";
+        int year = 2024;
+        String id = rt + " in the " + year;
+        System.out.println(id);
+
+    }
+}
