7c86aaee214c425f1564e8f4a9e0ed9b4b68593f
Fensyk <Dima-fech@yandex.ru>
2023-10-29 17:47:19 +0300

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,26 +1,26 @@
 package ru.job4j.converter;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ConverterTest {
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
 
     @Test
-    public void rubleToEuro() {
+    void whenConvert140RblThen2Euro() {
         float in = 140;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
         float eps = 0.0001f;
-        Assertions.assertEquals(expected, out, eps);
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
 
     @Test
-    public void rubleToDollar() {
+    void whenConvert180RblThen3Dollar() {
         float in = 180;
         float expected = 3;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
-        Assertions.assertEquals(expected, out, eps);
-
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
 }
