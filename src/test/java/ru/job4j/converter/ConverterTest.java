931886dc4983869fba955496a13aad2d2554d0cc
Fensyk <Dima-fech@yandex.ru>
2023-10-29 17:27:52 +0300

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,17 +1,17 @@
 package ru.job4j.converter;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class ConverterTest {
 
     @Test
     public void rubleToEuro() {
-        float in = 210;
-        float expected = 3;
+        float in = 140;
+        float expected = 2;
         float out = Converter.rubleToEuro(in);
         float eps = 0.0001f;
-        Assert.assertEquals(expected, out, eps);
+        Assertions.assertEquals(expected, out, eps);
     }
 
     @Test
@@ -20,6 +20,7 @@ public class ConverterTest {
         float expected = 3;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
-        Assert.assertEquals(expected, out, eps);
+        Assertions.assertEquals(expected, out, eps);
+
     }
 }
